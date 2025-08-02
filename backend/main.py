2b6c2df31759a85d25dd20aab1c55d01717df9a6
StriderNJE <stridernje@gmail.com>
2025-08-02 16:57:19 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -191,3 +191,4 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
         data={"sub": user.email}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
+
